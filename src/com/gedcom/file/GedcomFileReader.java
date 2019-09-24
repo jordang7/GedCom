9697125419683c686395e32c1ace16a102372536
Srikanth Madhava <srikanth.attur@gmail.com>
2019-09-23 22:39:57 -0400

    Gedccom processor

---
+++ src/com/gedcom/file/GedcomFileReader.java
@@ -0,0 +1,29 @@
+package com.gedcom.file;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Meghana on 9/12/2019.
+ */
+
+public class GedcomFileReader {
+    public List<String> gedcomReader(String filePath) {
+        List<String> gedcomeLines = new ArrayList<>();
+        try  {
+
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            String sCurrentLine;
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                gedcomeLines.add( sCurrentLine);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return gedcomeLines;
+    }
+}
